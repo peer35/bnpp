887391e68c12dca8b564dcd4929569cf9c5bed5c
Peter Vos <pjmvos@gmail.com>
2020-02-10 16:43:29 +0100

    solr 7.2+ local params problem

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -5,10 +5,10 @@ class CatalogController < ApplicationController
   include Blacklight::Catalog
   include Blacklight::Marc::Catalog
 
-	## PV: remove sms and cite
- 	CatalogController.blacklight_config.show.document_actions.delete(:sms)
-	CatalogController.blacklight_config.show.document_actions.delete(:citation)
-	CatalogController.blacklight_config.show.document_actions.delete(:email)
+  ## PV: remove sms and cite
+  CatalogController.blacklight_config.show.document_actions.delete(:sms)
+  CatalogController.blacklight_config.show.document_actions.delete(:citation)
+  CatalogController.blacklight_config.show.document_actions.delete(:email)
 
   configure_blacklight do |config|
     # default advanced config values
@@ -23,7 +23,7 @@ class CatalogController < ApplicationController
     #
     ## Class for converting Blacklight's url parameters to into request parameters for the search index
     config.search_builder_class = ::SearchBuilder
-		#
+    #
 
     #
     ## Model that maps search index responses to the blacklight response model
@@ -31,14 +31,14 @@ class CatalogController < ApplicationController
 
     ## Default parameters to send to solr for all search-like requests. See also SearchBuilder#processed_parameters
     config.default_solr_params = {
-      rows: 10
+        rows: 10
 
     }
-		#config.spell_max=10
+    #config.spell_max=10
 
-		config.add_field_configuration_to_solr_request! # for highlighting
+    config.add_field_configuration_to_solr_request! # for highlighting
 
-		config.index.respond_to.docx = true
+    config.index.respond_to.docx = true
 
 
     # solr path which will be added to solr base url before the other solr params.
@@ -50,12 +50,12 @@ class CatalogController < ApplicationController
     ## Default parameters to send on single-document requests to Solr. These settings are the Blackligt defaults (see SearchHelper#solr_doc_params) or
     ## parameters included in the Blacklight-jetty document requestHandler.
     #
-	config.default_document_solr_params = {
-		qt: 'document',
-		## These are hard-coded in the blacklight 'document' requestHandler
-		fl: '*',
-		rows: 1,
-		q: '{!term f=id v=$id}'
+    config.default_document_solr_params = {
+        qt: 'document',
+        ## These are hard-coded in the blacklight 'document' requestHandler
+        fl: '*',
+        rows: 1,
+        q: '{!term f=id v=$id}'
     }
 
     # solr field configuration for search results/index views
@@ -103,19 +103,19 @@ class CatalogController < ApplicationController
     # config.add_facet_field 'example_pivot_field', label: 'Pivot Field', :pivot => ['format', 'language_facet']
 
     # config.add_facet_field 'example_query_facet_field', label: 'Publish Date', :query => {
-       # :years_5 => { label: 'within 5 Years', fq: "pub_date:[#{Time.zone.now.year - 5 } TO *]" },
-       # :years_10 => { label: 'within 10 Years', fq: "pub_date:[#{Time.zone.now.year - 10 } TO *]" },
-       # :years_25 => { label: 'within 25 Years', fq: "pub_date:[#{Time.zone.now.year - 25 } TO *]" }
+    # :years_5 => { label: 'within 5 Years', fq: "pub_date:[#{Time.zone.now.year - 5 } TO *]" },
+    # :years_10 => { label: 'within 10 Years', fq: "pub_date:[#{Time.zone.now.year - 10 } TO *]" },
+    # :years_25 => { label: 'within 25 Years', fq: "pub_date:[#{Time.zone.now.year - 25 } TO *]" }
     # }
 
-	config.add_facet_field 'personen_sm', label: 'Persoon', sort: 'count', limit: 10, index_range: 'A'..'Z'
-	config.add_facet_field 'uitgave_sm', label: 'Plaats van Uitgave', sort: 'count', limit: 10, index_range: 'A'..'Z'
-	config.add_facet_field 'uitgever_sm', label: 'Uitgever', sort: 'count', limit: 10, index_range: 'A'..'Z'
-	config.add_facet_field 'eigenaar_sm', label: 'Eigenaar', sort: 'count', limit: 10, index_range: 'A'..'Z'
-	config.add_facet_field 'autopsie_sm', label: 'Autopsie', sort: 'count', limit: 10, index_range: 'A'..'Z'
-	config.add_facet_field 'categorie_sm', label: 'Categorie', limit: true, sort: 'index'
-	config.add_facet_field 'omvang_sm', label: 'Omvang per jaargang', limit: true, sort: 'index'
-	#config.add_facet_field 'achtergrond_sm', label: 'Achtergrond', limit: true
+    config.add_facet_field 'personen_sm', label: 'Persoon', sort: 'count', limit: 10, index_range: 'A'..'Z'
+    config.add_facet_field 'uitgave_sm', label: 'Plaats van Uitgave', sort: 'count', limit: 10, index_range: 'A'..'Z'
+    config.add_facet_field 'uitgever_sm', label: 'Uitgever', sort: 'count', limit: 10, index_range: 'A'..'Z'
+    config.add_facet_field 'eigenaar_sm', label: 'Eigenaar', sort: 'count', limit: 10, index_range: 'A'..'Z'
+    config.add_facet_field 'autopsie_sm', label: 'Autopsie', sort: 'count', limit: 10, index_range: 'A'..'Z'
+    config.add_facet_field 'categorie_sm', label: 'Categorie', limit: true, sort: 'index'
+    config.add_facet_field 'omvang_sm', label: 'Omvang per jaargang', limit: true, sort: 'index'
+    #config.add_facet_field 'achtergrond_sm', label: 'Achtergrond', limit: true
 
 
     # Have BL send all facet field names to Solr, which has been the default
@@ -125,38 +125,37 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the index (search results) view
     #   The ordering of the field names is the order of the display
-	config.add_index_field 'ondertitelc_s', label: 'Ondertitel', :highlight => true
-	config.add_index_field 'eigenaarc_s', label: 'Eigenaar'
-	config.add_index_field 'verschenen_s', label: 'Verschenen'
+    config.add_index_field 'ondertitelc_s', label: 'Ondertitel', :highlight => true
+    config.add_index_field 'eigenaarc_s', label: 'Eigenaar'
+    config.add_index_field 'verschenen_s', label: 'Verschenen'
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
     # config.add_show_field 'title_display', label: 'Title'
-	#config.add_show_field 'titel_s', label:'Titel'
-	config.add_show_field 'ondertitelc_s', label: 'Ondertitel', :helper_method => :linebreak_helper
-	config.add_show_field 'categorie_s', label: 'Categorie', :helper_method => :linebreak_helper
-	config.add_show_field 'verschenen_s', label: 'Verschenen', :helper_method => :linebreak_helper
-	config.add_show_field 'eigenaarc_s', label: 'Eigenaar', :helper_method => :linebreak_helper
-	config.add_show_field 'uitgeverc_s', label: 'Uitgever', :helper_method => :linebreak_helper
-	config.add_show_field 'drukkerc_s', label: 'Drukker', :helper_method => :linebreak_helper
-	config.add_show_field 'uitgavec_s', label: 'Plaats van Uitgave', :helper_method => :linebreak_helper
-	config.add_show_field 'frequentie_s', label: 'Frequentie', :helper_method => :linebreak_helper
-	config.add_show_field 'omvang_s', label: 'Omvang', :helper_method => :linebreak_helper
-	config.add_show_field 'formaat_s', label: 'Formaat', :helper_method => :linebreak_helper
-	config.add_show_field 'oplage_s', label: 'Oplage', :helper_method => :linebreak_helper
-	config.add_show_field 'prijzen_s', label: 'Prijzen', :helper_method => :linebreak_helper
-	config.add_show_field 'fotos_s', label: 'Foto\'s', :helper_method => :linebreak_helper
-	config.add_show_field 'tekeningen_s', label: 'Tekeningen', :helper_method => :linebreak_helper
-	config.add_show_field 'redactiec_s', label: 'Redactie', :helper_method => :linebreak_helper
-	config.add_show_field 'medewerkersc_s', label: 'Medewerkers', :helper_method => :linebreak_helper
-	config.add_show_field 'speciale_s', label: 'Speciale nummers', :helper_method => :linebreak_helper
-	config.add_show_field 'biblio_s', label: 'Bibliografische gegevens', :helper_method => :vv_helper
-	config.add_show_field 'autopsie_s', label: 'Autopsie', :helper_method => :linebreak_helper
-	config.add_show_field 'achtergrond_s', label: 'Achtergrond', :helper_method => :linebreak_helper
-	config.add_show_field 'literatuur_s', label: 'Literatuur', :helper_method => :linebreak_helper
-	config.add_show_field 'vindplaats_s', label: 'Vindplaats', :helper_method => :linebreak_helper
-	config.add_show_field 'bnpp_s', label: 'In BNPP'
-	
+    #config.add_show_field 'titel_s', label:'Titel'
+    config.add_show_field 'ondertitelc_s', label: 'Ondertitel', :helper_method => :linebreak_helper
+    config.add_show_field 'categorie_s', label: 'Categorie', :helper_method => :linebreak_helper
+    config.add_show_field 'verschenen_s', label: 'Verschenen', :helper_method => :linebreak_helper
+    config.add_show_field 'eigenaarc_s', label: 'Eigenaar', :helper_method => :linebreak_helper
+    config.add_show_field 'uitgeverc_s', label: 'Uitgever', :helper_method => :linebreak_helper
+    config.add_show_field 'drukkerc_s', label: 'Drukker', :helper_method => :linebreak_helper
+    config.add_show_field 'uitgavec_s', label: 'Plaats van Uitgave', :helper_method => :linebreak_helper
+    config.add_show_field 'frequentie_s', label: 'Frequentie', :helper_method => :linebreak_helper
+    config.add_show_field 'omvang_s', label: 'Omvang', :helper_method => :linebreak_helper
+    config.add_show_field 'formaat_s', label: 'Formaat', :helper_method => :linebreak_helper
+    config.add_show_field 'oplage_s', label: 'Oplage', :helper_method => :linebreak_helper
+    config.add_show_field 'prijzen_s', label: 'Prijzen', :helper_method => :linebreak_helper
+    config.add_show_field 'fotos_s', label: 'Foto\'s', :helper_method => :linebreak_helper
+    config.add_show_field 'tekeningen_s', label: 'Tekeningen', :helper_method => :linebreak_helper
+    config.add_show_field 'redactiec_s', label: 'Redactie', :helper_method => :linebreak_helper
+    config.add_show_field 'medewerkersc_s', label: 'Medewerkers', :helper_method => :linebreak_helper
+    config.add_show_field 'speciale_s', label: 'Speciale nummers', :helper_method => :linebreak_helper
+    config.add_show_field 'biblio_s', label: 'Bibliografische gegevens', :helper_method => :vv_helper
+    config.add_show_field 'autopsie_s', label: 'Autopsie', :helper_method => :linebreak_helper
+    config.add_show_field 'achtergrond_s', label: 'Achtergrond', :helper_method => :linebreak_helper
+    config.add_show_field 'literatuur_s', label: 'Literatuur', :helper_method => :linebreak_helper
+    config.add_show_field 'vindplaats_s', label: 'Vindplaats', :helper_method => :linebreak_helper
+    config.add_show_field 'bnpp_s', label: 'In BNPP'
 
 
     # "fielded" search configuration. Used by pulldown among other places.
@@ -185,60 +184,60 @@ class CatalogController < ApplicationController
     # # of Solr search fields.
 
     # config.add_search_field('title') do |field|
-      # # solr_parameters hash are sent to Solr as ordinary url query params.
-      # field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
+    # # solr_parameters hash are sent to Solr as ordinary url query params.
+    # field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
 
-      # # :solr_local_parameters will be sent using Solr LocalParams
-      # # syntax, as eg {! qf=$title_qf }. This is neccesary to use
-      # # Solr parameter de-referencing like $title_qf.
-      # # See: http://wiki.apache.org/solr/LocalParams
-      # field.solr_local_parameters = {
-        # qf: '$title_qf',
-        # pf: '$title_pf'
-      # }
+    # # :solr_local_parameters will be sent using Solr LocalParams
+    # # syntax, as eg {! qf=$title_qf }. This is neccesary to use
+    # # Solr parameter de-referencing like $title_qf.
+    # # See: http://wiki.apache.org/solr/LocalParams
+    # field.solr_local_parameters = {
+    # qf: '$title_qf',
+    # pf: '$title_pf'
+    # }
     # end
 
     config.add_search_field('personen_sm', label: 'Personen') do |field|
-      field.solr_parameters = { :'spellcheck.dictionary' => 'personen' }
-      field.solr_local_parameters = {
-        qf: '$personen_qf',
-        pf: '$personen_pf'
+      field.solr_parameters = {
+          :'spellcheck.dictionary' => 'personen',
+          qf: "'${personen_qf}'",
+          pf: "'${personen_pf}'"
       }
     end
 
-		config.add_search_field('titel_s', label: 'Titel') do |field|
-			field.solr_parameters = { :'spellcheck.dictionary' => 'titel' }
-			field.solr_local_parameters = {
-					qf: '$titel_qf',
-					pf: '$titel_pf'
-			}
-		end
+    config.add_search_field('titel_s', label: 'Titel') do |field|
+      field.solr_parameters = {
+          :'spellcheck.dictionary' => 'titel',
+          qf: "'${titel_qf}'",
+          pf: "'${titel_pf}'"
+      }
+    end
 
-		config.add_search_field('eigenaar_s', label: 'Eigenaar') do |field|
-			field.solr_parameters = { :'spellcheck.dictionary' => 'eigenaar' }
-			field.solr_local_parameters = {
-					qf: '$eigenaar_qf',
-					pf: '$eigenaar_pf'
-			}
-		end
+    config.add_search_field('eigenaar_s', label: 'Eigenaar') do |field|
+      field.solr_parameters = {
+          :'spellcheck.dictionary' => 'eigenaar',
+          qf: "'${eigenaar_qf}'",
+          pf: "'${eigenaar_pf}'"
+      }
+    end
 
-  	config.add_search_field('verschenen_dtr', label: 'Jaar') do |field|
-			field.solr_local_parameters = {
-					qf: '$verschenen_qf',
-					pf: '$verschenen_pf'
-			}
-		end
+    config.add_search_field('verschenen_dtr', label: 'Jaar') do |field|
+      field.solr_parameters = {
+          qf: "'${verschenen_qf}'",
+          pf: "'${verschenen_pf}'"
+      }
+    end
 
     # # Specifying a :qt only to show it's possible, and so our internal automated
     # # tests can test it. In this case it's the same as
     # # config[:default_solr_parameters][:qt], so isn't actually neccesary.
     # config.add_search_field('subject') do |field|
-      # field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
-      # field.qt = 'search'
-      # field.solr_local_parameters = {
-        # qf: '$subject_qf',
-        # pf: '$subject_pf'
-      # }
+    # field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
+    # field.qt = 'search'
+    # field.solr_local_parameters = {
+    # qf: '$subject_qf',
+    # pf: '$subject_pf'
+    # }
     # end
 
     # "sort results by" select (pulldown)
@@ -246,10 +245,10 @@ class CatalogController < ApplicationController
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case).
     #config.add_sort_field 'score desc, pub_date_sort desc, titel_sort asc', label: 'relevance'
-	  config.add_sort_field 'score desc, titel_sort asc', label: 'relevantie'
-	  config.add_sort_field 'titel_sort asc', label: 'titel'
+    config.add_sort_field 'score desc, titel_sort asc', label: 'relevantie'
+    config.add_sort_field 'titel_sort asc', label: 'titel'
     config.add_sort_field 'pub_date_sort asc, titel_sort asc', label: 'jaar (oplopend)'
-		config.add_sort_field 'pub_date_sort desc, titel_sort asc', label: 'jaar (aflopend)'
+    config.add_sort_field 'pub_date_sort desc, titel_sort asc', label: 'jaar (aflopend)'
 
 
     # If there are more than this many search results, no spelling ("did you
